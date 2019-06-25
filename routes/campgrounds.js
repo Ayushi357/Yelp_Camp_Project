8c4db4c2b85de06279da82737f7fbc1554ff3f7e
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-25 10:26:22 +0530

    Prevented an unauthenticated user from creating campground and saved username+id to newly created campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -15,12 +15,21 @@ router.get("/", function(req, res) {
 });
 
 // CREATE - add new campground to DB
-router.post("/", function(req, res) {
+router.post("/", isloggedIn, function(req, res) {
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.image;
   var description = req.body.description;
-  var newCampground = { name: name, image: image, description: description };
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  var newCampground = {
+    name: name,
+    image: image,
+    description: description,
+    author: author
+  };
   // Create a new campground and save to DB
   Campground.create(newCampground, function(err, newlyCreated) {
     if (err) {
@@ -33,7 +42,7 @@ router.post("/", function(req, res) {
 });
 
 // NEW - show form to create new campground
-router.get("/new", function(req, res) {
+router.get("/new", isloggedIn, function(req, res) {
   res.render("campgrounds/new");
 });
 
@@ -52,5 +61,13 @@ router.get("/:id", function(req, res) {
       }
     });
 });
+
+// middleware
+function isloggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 module.exports = router;
