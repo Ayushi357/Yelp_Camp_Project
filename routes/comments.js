b91ab7fb8ff5b81285eb58037de9ac3598758cc6
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-25 18:29:44 +0530

    Added Destroy Comments Route & Delete Button

--- routes/comments.js
+++ routes/comments.js
@@ -72,6 +72,18 @@ router.put("/:comment_id", function(req, res) {
   });
 });
 
+// COMMENT DESTROY ROUTE
+router.delete("/:comment_id", function(req, res) {
+  //findByIdRemove
+  Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 // middleware
 function isloggedIn(req, res, next) {
   if (req.isAuthenticated()) {
