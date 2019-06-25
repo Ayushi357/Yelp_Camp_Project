6671d30d1a4ffdc5825b2f6bcf5d36092c5d0f92
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-25 18:14:33 +0530

    Added Edit/Update Route for comments & an edit button

--- routes/comments.js
+++ routes/comments.js
@@ -44,6 +44,34 @@ router.post("/", isloggedIn, function(req, res) {
   });
 });
 
+// COMMENT EDIT ROUTE
+router.get("/:comment_id/edit", function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, foundComment) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {
+        campground_id: req.params.id,
+        comment: foundComment
+      });
+    }
+  });
+});
+
+// COMMENT UPDATE
+router.put("/:comment_id", function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(
+    err,
+    updatedComment
+  ) {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 // middleware
 function isloggedIn(req, res, next) {
   if (req.isAuthenticated()) {
