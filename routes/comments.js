8132efcdce44285f045b977ee43f4c19a438a874
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-25 09:59:37 +0530

    Associated users & comments, saved author name to a comment automatically

--- routes/comments.js
+++ routes/comments.js
@@ -27,6 +27,11 @@ router.post("/", isloggedIn, function(req, res) {
         if (err) {
           console.log(err);
         } else {
+          // add username and id to comment
+          comment.author.id = req.user._id;
+          comment.author.username = req.user.username;
+          // save comment
+          comment.save();
           // create new comment
           // connect new comment to campground
           campground.comments.push(comment);
