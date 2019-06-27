5c039f7be472d18dd46afd341b11b42a16c357ff
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-27 12:46:12 +0530

    Installed and configured connect-flash & added bootstrap alerts to header

--- middleware/index.js
+++ middleware/index.js
@@ -10,17 +10,20 @@ middlewareObj.checkCampgroundOwnership =
   if (req.isAuthenticated()) {
     Campground.findById(req.params.id, function(err, foundCampground) {
       if (err) {
+        req.flash('error', 'Campground not found');
         res.redirect('back');
       } else {
         // does user own the campground?
         if (foundCampground.author.id.equals(req.user._id)) {
           next();
         } else {
+          req.flash('error', 'You don\'t have permission to do that');
           res.redirect('back');
         }
       }
     });
   } else {
+    req.flash('error', 'You need to be logged in to do that.');
     res.redirect('back');
   }
 }
@@ -36,11 +39,13 @@ middlewareObj.checkCampgroundOwnership =
         if (foundComment.author.id.equals(req.user._id)) {
           next();
         } else {
+          req.flash('error', 'You don\'t have permission to do that.');
           res.redirect('back');
         }
       }
     });
   } else {
+    req.flash('error', 'You need to be logged in to do that.');
     res.redirect('back');
   }
 }
@@ -50,6 +55,7 @@ middlewareObj.checkCampgroundOwnership =
   if (req.isAuthenticated()) {
     return next();
   }
+  req.flash('error', 'You need to be logged in to do that.');
   res.redirect('/login');
 }
 
--- routes/comments.js
+++ routes/comments.js
@@ -26,6 +26,7 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
     } else {
       Comment.create(req.body.comment, function(err, comment) {
         if (err) {
+          req.flash('error', 'Something went wrong!');
           console.log(err);
         } else {
           // add username and id to comment
@@ -38,6 +39,7 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
           campground.comments.push(comment);
           campground.save();
           // redirect campground show page
+          req.flash('success', 'Successfully added comment');
           res.redirect('/campgrounds/' + campground._id);
         }
       });
@@ -81,6 +83,7 @@ router.delete(
         if (err) {
           res.redirect('back');
         } else {
+          req.flash('success', 'Comment deleted!');
           res.redirect('/campgrounds/' + req.params.id);
         }
       });
