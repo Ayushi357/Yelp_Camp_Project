dffa92c20e4e519892e5675253981da1a2e58e0e
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-28 14:37:45 +0530

    Added Admin User Role Authorization

--- routes/index.js
+++ routes/index.js
@@ -1,50 +1,56 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var passport = require('passport');
-var User = require('../models/user');
+var passport = require("passport");
+var User = require("../models/user");
 
 // root route
-router.get('/', function(req, res) {
-  res.render('landing');
+router.get("/", function(req, res) {
+  res.render("landing");
 });
 
 // show register form
-router.get('/register', function(req, res) {
-  res.render('register', {page: 'register'});
+router.get("/register", function(req, res) {
+  res.render("register", { page: "register" });
 });
 
 // handle sign up logic
-router.post('/register', function(req, res) {
-  var newUser = new User({username: req.body.username});
+router.post("/register", function(req, res) {
+  var newUser = new User({ username: req.body.username });
+  if (req.body.adminCode === "123456") {
+    newUser.isAdmin = true;
+  }
   User.register(newUser, req.body.password, function(err, user) {
     if (err) {
       console.log(err);
-      return res.render('register', {error: err.message});
+      return res.render("register", { error: err.message });
     }
-    passport.authenticate('local')(req, res, function() {
-      req.flash('success', 'Welcome to YelpCamp ' + user.username);
-      res.redirect('/campgrounds');
+    passport.authenticate("local")(req, res, function() {
+      req.flash("success", "Welcome to YelpCamp " + user.username);
+      res.redirect("/campgrounds");
     });
   });
 });
 
 // show login form
-router.get('/login', function(req, res) {
-  res.render('login', {page: 'login'});
+router.get("/login", function(req, res) {
+  res.render("login", { page: "login" });
 });
 
 // handling login logic
 router.post(
-    '/login',
-    passport.authenticate(
-        'local', {successRedirect: '/campgrounds', failureRedirect: '/login'}),
-    function(req, res) {});
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }),
+  function(req, res) {}
+);
 
 // logout route
-router.get('/logout', function(req, res) {
+router.get("/logout", function(req, res) {
   req.logOut();
-  req.flash('success', 'Logged you Out!');
-  res.redirect('/campgrounds');
+  req.flash("success", "Logged you Out!");
+  res.redirect("/campgrounds");
 });
 
 module.exports = router;
