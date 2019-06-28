49bf85329b398006fd39a5d96ae97cd7e33ae98e
Ayushi Agrawal <ayushi357agrawal@gmail.com>
2019-06-28 19:46:32 +0530

    Created fuzzy search with express and mongoose

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-require('dotenv').config();
+require("dotenv").config();
 
-var express = require('express'), app = express(),
-    bodyParser = require('body-parser'), mongoose = require('mongoose'),
-    flash = require('connect-flash'), passport = require('passport'),
-    LocalStrategy = require('passport-local'),
-    methodOverride = require('method-override'),
-    Campground = require('./models/campground'),
-    Comment = require('./models/comment'), User = require('./models/user'),
-    seedDB = require('./seeds');
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"),
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
 // requiring routes
-var commentRoutes = require('./routes/comments'),
-    campgroundRoutes = require('./routes/campgrounds'),
-    indexRoutes = require('./routes/index');
+var commentRoutes = require("./routes/comments"),
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
-mongoose.set('useFindAndModify', false);
-mongoose.connect(
-    'mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
-app.use(bodyParser.urlencoded({extended: true}));
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {
+  useNewUrlParser: true
+});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
 app.use(flash());
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 
 // seedDB(); // seed the database
 
 // PASSPORT CONFIGURATION
-app.use(require('express-session')({
-  secret: 'All around me are familiar faces.',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "All around me are familiar faces.",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -41,15 +50,15 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next) {
   res.locals.currentUser = req.user;
-  res.locals.error = req.flash('error')
-  res.locals.success = req.flash('success');
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
   next();
 });
 
-app.use('/', indexRoutes);
-app.use('/campgrounds', campgroundRoutes);
-app.use('/campgrounds/:id/comments', commentRoutes);
+app.use("/", indexRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
 
 app.listen(3000, function() {
-  console.log('THE YELP CAMP SERVER HAS STARTED!');
+  console.log("THE YELP CAMP SERVER HAS STARTED!");
 });
